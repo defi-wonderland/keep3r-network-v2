505d194c4b662dd0cc59cf397ecec14dca423829
Ashitaka <ashitaka@defi.sucks>
2024-06-07 14:58:12 -0300

    fix: lint

--- solidity/for-test/BasicJob.sol
+++ solidity/for-test/BasicJob.sol
@@ -1,20 +1,20 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.4 <0.9.0;
 
-import "../interfaces/IKeep3r.sol";
+import '../interfaces/IKeep3r.sol';
 
 contract BasicJob {
-    error KeeperNotValid();
+  error KeeperNotValid();
 
-    address public keep3r;
-    uint256 public nonce;
-    uint256[] public array;
+  address public keep3r;
+  uint256 public nonce;
+  uint256[] public array;
 
-    constructor(address _keep3r) {
-        keep3r = _keep3r;
-    }
+  constructor(address _keep3r) {
+    keep3r = _keep3r;
+  }
 
-    function test() external {
-        array.push(1);
-    }
+  function test() external {
+    array.push(1);
+  }
 }
--- solidity/for-test/JobForTest.sol
+++ solidity/for-test/JobForTest.sol
@@ -1,35 +1,35 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.4 <0.9.0;
 
-import "../interfaces/IKeep3r.sol";
+import '../interfaces/IKeep3r.sol';
 
 contract JobForTest {
-    error InvalidKeeper();
+  error InvalidKeeper();
 
-    address public keep3r;
-    uint256 public nonce;
+  address public keep3r;
+  uint256 public nonce;
 
-    constructor(address _keep3r) {
-        keep3r = _keep3r;
+  constructor(address _keep3r) {
+    keep3r = _keep3r;
+  }
+
+  function work() external {
+    if (!IKeep3r(keep3r).isKeeper(msg.sender)) revert InvalidKeeper();
+
+    for (uint256 i; i < 1000; i++) {
+      nonce++;
     }
 
-    function work() external {
-        if (!IKeep3r(keep3r).isKeeper(msg.sender)) revert InvalidKeeper();
+    IKeep3r(keep3r).worked(msg.sender);
+  }
 
-        for (uint256 i; i < 1000; i++) {
-            nonce++;
-        }
+  function workHard(uint256 _factor) external {
+    if (!IKeep3r(keep3r).isKeeper(msg.sender)) revert InvalidKeeper();
 
-        IKeep3r(keep3r).worked(msg.sender);
+    for (uint256 i; i < 1000 * _factor; i++) {
+      nonce++;
     }
 
-    function workHard(uint256 _factor) external {
-        if (!IKeep3r(keep3r).isKeeper(msg.sender)) revert InvalidKeeper();
-
-        for (uint256 i; i < 1000 * _factor; i++) {
-            nonce++;
-        }
-
-        IKeep3r(keep3r).worked(msg.sender);
-    }
+    IKeep3r(keep3r).worked(msg.sender);
+  }
 }
